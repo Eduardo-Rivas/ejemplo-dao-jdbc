4105460b0bd54705b1bd9b01b771222daada8379
Eduardo-Rivas <edumeru46@gmail.com>
2020-06-17 09:43:58 -0300

    Clase Departamentos y Principal

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,13 @@
+package aplicacion;
+
+import model.entidades.Departamentos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+       Departamentos depar = new Departamentos(1, "Zapateria");
+       System.out.println(depar);
+ 
+	}
+
+}
---
+++ src/model/entidades/Departamentos.java
@@ -0,0 +1,71 @@
+package model.entidades;
+
+import java.io.Serializable;
+
+public class Departamentos implements Serializable{
+	//--Definimos Nro. de Version Padron Serializable --//
+	private static final long serialVersionUID = 1L;
+	
+	//--Definimos los Atributos--//
+	private Integer id;
+	private String nombre;
+
+	//--Definimos constructor Padron--//
+	public Departamentos() {
+	}
+	//--Definimos constructor con Argumentos--//
+	public Departamentos(Integer id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	//--Definimos Getters y Setters--//
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	//--Definimos de Comparacion hashCode e Equals --//
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamentos other = (Departamentos) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	//--Definimos Metodo de Salia de Datos--//
+	@Override
+	public String toString() {
+		return "Departamentos :" + id + " - Nombre :" + nombre;
+	}
+
+	
+	
+	
+	
+}
