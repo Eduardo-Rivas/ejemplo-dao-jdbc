b6ebdd61f562ea8fa5e1d294a7f51b8b6b07e8b6
Eduardo-Rivas <edumeru46@gmail.com>
2020-06-17 13:26:38 -0300

    Clase Vendedores

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,12 +1,17 @@
 package aplicacion;
 
+
+import java.util.Date;
 import model.entidades.Departamentos;
+import model.entidades.Vendedores;
 
 public class Principal {
 
 	public static void main(String[] args) {
-       Departamentos depar = new Departamentos(1, "Zapateria");
-       System.out.println(depar);
+       Departamentos dep = new Departamentos(1, "Zapateria");
+       Vendedores ven = new Vendedores(21, "Bob", "bob@gmail.com", new Date(), 7000.0, dep);
+       
+       System.out.println(ven);
  
 	}
 
---
+++ src/model/entidades/Vendedores.java
@@ -0,0 +1,102 @@
+package model.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+ 
+public class Vendedores implements Serializable{
+	//--Versionamiento de Serializable--//
+	private static final long serialVersionUID = 1L;
+	
+	// --Definimos los Atributos--//
+	private Integer id;
+	private String nombre;
+	private String email;
+	private Date fecha;
+	private Double SalarioBase;
+
+	// --Composicion de los Objs.--//
+	private Departamentos depart;
+
+	// --Costructor Padron--//
+	public Vendedores() {
+	}
+	// --Costructor con Argumentos--//
+	public Vendedores(Integer id, String nombre, String email, Date fecha, Double salarioBase, Departamentos depart) {
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.fecha = fecha;
+		SalarioBase = salarioBase;
+		this.depart = depart;
+	}
+	
+	//--Getters y Setters--//
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Double getSalarioBase() {
+		return SalarioBase;
+	}
+	public void setSalarioBase(Double salarioBase) {
+		SalarioBase = salarioBase;
+	}
+	public Departamentos getDepart() {
+		return depart;
+	}
+	public void setDepart(Departamentos depart) {
+		this.depart = depart;
+	}
+	
+	//--hashCode e Equals--//
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedores other = (Vendedores) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	//--Metodo toString--//
+	@Override
+	public String toString() {
+		return "Vendedores [id=" + id + ", nombre=" + nombre + ", email=" + email + ", fecha=" + fecha
+				+ ", SalarioBase=" + SalarioBase + ", depart=" + depart + "]";
+	}
+}
